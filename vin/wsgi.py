4d3e7af9ec6019ead250741bd59cc62126359f56
Ivan <ashikhin1998@gmail.com>
2023-11-07 11:22:39 +0600

    Create User Model and Admin

--- vin/wsgi.py
+++ vin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for VIN_decoder_proj project.
+WSGI config for vin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VIN_decoder_proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vin.settings')
 
 application = get_wsgi_application()
