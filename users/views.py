6d5e726e67060c8930883746cfe9fe3dfc432591
Ivan <ashikhin1998@gmail.com>
2023-11-20 12:12:38 +0600

    Rename html file

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from api.users.serializers import LoginSerializer
 
 
 def login_form_view(request):
-    return render(request, 'login.html')
+    return render(request, 'index.html')
 
 
 @api_view(['GET', 'POST'])
@@ -24,4 +24,4 @@ def login_user(request):
             access_token = str(refresh.access_token)
             request.session['access_token'] = access_token
             return redirect('new_decode_vin', user_id=user.id)
-    return render(request, 'login.html')
+    return render(request, 'index.html')
