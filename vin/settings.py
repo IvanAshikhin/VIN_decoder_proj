021654683eafa7aa4c4e5228f217a050cc8e51ec
Ivan <ashikhin1998@gmail.com>
2023-11-29 14:09:12 +0600

    Add whitenoise to collect static

--- vin/settings.py
+++ vin/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
@@ -174,6 +175,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
