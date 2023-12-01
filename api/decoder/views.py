5585608d42cd7fb682a234d9db96ca8941f54cc5
Ivan <ashikhin1998@gmail.com>
2023-12-01 12:35:11 +0600

    Added a check that if the length of the vin code is shorter or longer than 17 characters

--- api/decoder/views.py
+++ api/decoder/views.py
@@ -30,19 +30,17 @@ class DecodeVinAPIView(APIView):
     )
     def post(self, request):
         vin_code = self.request.data.get("vin_code")
-        if vin_code:
-            car = get_car(vin_code)
-            if car:
-                serialized_car = CarSerializer(car).data
-                return Response(
-                    {"car": serialized_car, "user_id": self.request.user.id}
-                )
-            else:
-                return Response(
-                    {"error": "Car not found for the provided VIN code"},
-                    status=status.HTTP_404_NOT_FOUND,
-                )
+        if not vin_code or len(vin_code) != 17:
+            return Response(
+                {"error": "Invalid VIN code. Must be 17 characters long."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        car = get_car(vin_code)
+        if car:
+            serialized_car = CarSerializer(car).data
+            return Response({"car": serialized_car, "user_id": self.request.user.id})
         else:
             return Response(
-                {"error": "VIN code not provided"}, status=status.HTTP_400_BAD_REQUEST
+                {"error": "Car not found for the provided VIN code"},
+                status=status.HTTP_404_NOT_FOUND,
             )
--- decoder/forms.py
+++ decoder/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class VinDecodeForm(forms.Form):
-    vin_code = forms.CharField(label="VIN Code", max_length=17)
+    vin_code = forms.CharField(label="VIN Code", max_length=17, min_length=17)
